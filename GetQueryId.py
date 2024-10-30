9bc59cccfb88c182d587e8f340cbd75208a33c50
Tran Danh Doanh <nhimcaptin@gmail.com>
2024-10-30 10:34:53 +0700

    update proxy chrome

--- GetQueryId.py
+++ GetQueryId.py
@@ -10,23 +10,30 @@ from utils import (
     chrome_driver_path,
     sort_data_by_user_id,
 )
-
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
 # import json
 
-# https://web.telegram.org/k/#@TimeFarmCryptoBot
+
 urlQuery = input("Nhập url con cần lấy query: ")
 countProject = int(input("Nhập số profile: "))
 numberThreads = int(input("Nhập số luồng: "))
+sort_choice = input("Bạn có muốn thực hiện sort không? (y/n): ")
+proxy = []
+
 with open("data.txt", "w", encoding="utf-8") as file:
     pass
+with open("proxy.txt", "r", encoding="utf-8") as file:
+    for line in file:
+        proxy.append(line.strip())
 
 
 def run_profile(i):
     user_data_dir = getUserDataDir(i)
     chrome_options = Options()
     chrome_options.add_argument(f"user-data-dir={user_data_dir}")
+    if len(proxy) > 0:
+        chrome_options.add_argument(f"--proxy-server=http://{proxy[i-1]}")
     chrome_options.add_argument("--window-size=250,600")
     # chrome_options.add_argument("--disable-web-security")
     # chrome_options.add_argument("--no-sandbox")
@@ -72,8 +79,7 @@ def run_profile(i):
 
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=numberThreads) as executor:
-    executor.map(run_profile, range(6, countProject + 1))
+    executor.map(run_profile, range(1, countProject + 1))
 
-sort_choice = input("Bạn có muốn thực hiện sort không? (y/n): ")
 if sort_choice.lower() == "y":
     sort_data_by_user_id()
